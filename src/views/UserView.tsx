010ec08bafdf0448c4f1a26849536c32e6fa71c7
mogiel <mogielski@protonmail.com>
2022-07-14 10:40:27 +0200

    0.2.1 Update for production

--- src/views/UserView.tsx
+++ src/views/UserView.tsx
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import { Input } from '@chakra-ui/react';
+import { UserReturn } from 'types';
 
 export const UserView = () => {
-    const [name, setName] = useState('');
+    const [content, setContent] = useState<UserReturn | null>(null);
 
     const [token, setToken] = useState('')
 
@@ -11,10 +12,6 @@ export const UserView = () => {
             async () => {
                 await fetch('http://localhost:3001/user/login', {
                     credentials: 'include',
-                    headers: {
-                        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEzNTVlOTI2LWM5NzItNDNkMy05NDk4LWJkMDMyNjc0MTM4ZCIsImlhdCI6MTY1NzcwMjU2MiwiZXhwIjoxNjU3Nzg4OTYyfQ.9j0c4EeifxxBlAAch6ewRN7Em4f7OgD2DHWkYOxZL8E',
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    }
                 });
             }
         )();
@@ -31,17 +28,20 @@ export const UserView = () => {
         })
             .then((res) => res.json());
 
+        setContent(data)
+
+
         if (data) {
-            setName(data.username)
+            setContent(data)
         } else {
-            setToken('')
+            setContent(null)
         }
 
     }
 
 
     return (<>
-        {name ? <h1>zalogowany</h1> : <h1>nie zalogowany</h1>}
+        {content ? <h1>zalogowany {content.username} o id: {content.id}</h1> : <h1>nie zalogowany</h1>}
         <br/>
         <h1>Wklej token ze strony logowania</h1>
         <Input type={"text"} onChange={e => setToken(e.target.value)} value={token}/>
