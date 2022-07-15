a6d6abe9a176c776ee6888a1123e4b5efab15260
mogiel <mogielski@protonmail.com>
2022-07-15 18:11:03 +0200

    0.2.2 Update Fetch for production, UserView update for production

--- src/utils/Fetch/Fetch.ts
+++ src/utils/Fetch/Fetch.ts
@@ -6,7 +6,7 @@ type MethodChoice = 'POST' | 'GET' | 'DELETE' | 'PUT' | 'PATCH'
  */
 export class FetchOperator {
 
-    private readonly URL_API = "http://localhost:3001/"
+    private readonly URL_API = "https://mogiel89.smallhost.pl:3001/"
     private readonly mainLink: string;
 
     constructor(link: string) {
@@ -76,11 +76,3 @@ export class FetchOperator {
     }
 
 }
-
-/**
- * Przykładowe użycie dla POST:
- *
- * const data = await new FetchOperator('gift')
- * const dataRes = await data.run('POST','', form)
- *
- */
